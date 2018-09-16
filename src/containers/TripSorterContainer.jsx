ac6a39aeabc61434d93ef469c1b0815b470ed910
shruthi <shruthisrikantha@yahoo.com>
2018-09-16 10:30:54 +0400

    code clen up. Signed-off-by: shruthi <shruthisrikantha@yahoo.com>

--- src/containers/TripSorterContainer.jsx
+++ src/containers/TripSorterContainer.jsx
@@ -22,29 +22,19 @@ class TripSorterContainer extends React.Component {
     }
 
     handleCalculate(from, to, cheapest, fastest){
-        console.log('from'+ from)
-        console.log('to'+ to)
-        console.log('cheapest'+ cheapest)
-        console.log('fastest'+ fastest)
-        console.log(this.props.tripsorterdata.cheapestCities)
         let data = cheapest ? this.props.tripsorterdata.cheapestCities : this.props.tripsorterdata.fastestCities
         let res  = dijkstra(data, from, to)
-        console.log("res :"+ JSON.stringify(res))
         this.props.dispatch(setIsSearch(false))
         this.props.dispatch(setSearchResult(res))
     }
 
     handleReset(){
-        console.log("@handleReset")
         this.props.dispatch(setIsSearch(true))
     }
 
     render() {
         const { dispatch, tripsorterdata, searchVal } = this.props
-        console.log(this.props.searchVal+"this.props.searchVal")
-        console.log(" uniqueCities from reducer " + JSON.stringify(tripsorterdata.uniqueCities))
-        console.log(" cheapestCities from reducer " + JSON.stringify(tripsorterdata.cheapestCities))
-       
+    
         return (
             <div className="container-wrapper">
                 <h1 className="container-header">Trip Sorter</h1>
